f18243d971d64021d1a2e77d78d43b4502b1234c
mamba <nbapankui@163.com>
2021-08-16 22:24:42 +0800

    完成通过docker-compose 来初始化naocs 表结构

--- mamba-out/mamba-service/lakers-family-service/src/main/java/space/lakers/LakersIndexController.java
+++ mamba-out/mamba-service/lakers-family-service/src/main/java/space/lakers/LakersIndexController.java
@@ -1,5 +1,9 @@
 package space.lakers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author pankui
  * @date 2021/8/14
@@ -8,5 +12,12 @@ package space.lakers;
  * </pre>
  */
 
+@RestController
+@RequestMapping("/api/lakers")
 public class LakersIndexController {
+
+    @GetMapping("/hi")
+    public String hi() {
+        return "湖人总冠军";
+    }
 }
--- mamba-out/mamba-service/mamba-gateway/src/main/java/space/mamba/Application.java
+++ mamba-out/mamba-service/mamba-gateway/src/main/java/space/mamba/Application.java
@@ -1,14 +1,17 @@
 package space.mamba;
 
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author liyan16
  * @create 2020/5/9 11:59
  */
-//@SpringBootApplication(scanBasePackages = "space.mamba")
+@SpringBootApplication(scanBasePackages = "space.mamba")
 public class Application {
 
-	public static void main(String[] args) {
-	//	SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
